b64e890e5d3e7309947bdd5a89c0acf9f801ad49
Praveen Kumar <praveenscience@gmail.com>
2016-04-03 03:16:54 +0100

    Added quick-search in documentation.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
-$('.code-preview').hide();
+    $('.code-preview').hide();
     $('.show-code').click(function (e) {
         $(this).children('.name').toggle();
         $(this).children('.code-preview').toggle();
         e.stopPropagation();
         return false;
+    });
+    $("#quick-search").keyup(function () {
+        var srch = $(this).val().trim();
+        $(".icon-preview-box").hide()
+            .filter(function () {
+                return $(this).html().trim().indexOf(srch) != -1;
+            }).show();
     });
 });
