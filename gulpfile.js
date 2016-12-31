5be829fc1d083a58d706e3cfd451fe85843ee0f1
Sabbir Ahmed <sabbiirr@gmail.com>
2016-12-31 22:49:08 +0600

    Revert "added minify version and minify task"

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
-var gulp    = require('gulp'),
-    sass    = require('gulp-sass'),
-    nano    = require('gulp-cssnano'),
-    rename  = require('gulp-rename'),
-    less    = require('gulp-less');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    less = require('gulp-less');
 
 var paths = {
     less: 'less/simple-line-icons.less',
@@ -22,23 +20,6 @@ gulp.task('sass', function(){
     .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('minify-less', function() {
-    return gulp.src(paths.less)
-    .pipe(less())
-    .pipe(nano())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(paths.build));
-});
-
-gulp.task('minify-sass', function() {
-    return gulp.src(paths.sass)
-    .pipe(sass())
-    .pipe(nano())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(paths.build));
-});
-
 gulp.task('test', ['less', 'sass']);
-gulp.task('minify', ['minify-less', 'minify-sass']);
 
-gulp.task('default', ['test', 'minify']);
+gulp.task('default', ['test']);
